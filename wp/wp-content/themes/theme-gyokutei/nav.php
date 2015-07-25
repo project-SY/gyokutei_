25d67d1d994f2f7669f446b2c11c89dadf33e65b
山﨑　旭 <b2w51134.asahi@gmail.com>
2015-07-26 04:15:37 +0900

    modefied_navigation

--- wp/wp-content/themes/theme-gyokutei/nav.php
+++ wp/wp-content/themes/theme-gyokutei/nav.php
@@ -7,13 +7,13 @@
           <a class="close-btn btn" href="#"><img src="<?php echo get_template_directory_uri(); ?>/images/btn-menu-off-sp.png" alt=""></a>
         </div>
         <ul>
-          <li><a href="<?php echo home_url(); ?>"><img src="<?php echo get_template_directory_uri(); ?>/images/txt-sidebar-top.png" alt="トップ"></a></li>
+          <li><a href="top"><img src="<?php echo get_template_directory_uri(); ?>/images/txt-sidebar-top.png" alt="トップ"></a></li>
           <li><a href="bath"><img src="<?php echo get_template_directory_uri(); ?>/images/txt-sidebar-bath.png" alt="お風呂"></a></li>
           <li><a href="room"><img src="<?php echo get_template_directory_uri(); ?>/images/txt-sidebar-room.png" alt="お部屋"></a></li>
-          <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/txt-sidebar-food.png" alt="お料理"></a></li>
+          <li><a href="food"><img src="<?php echo get_template_directory_uri(); ?>/images/txt-sidebar-food.png" alt="お料理"></a></li>
           <li><a href="reservation"><img src="<?php echo get_template_directory_uri(); ?>/images/txt-sidebar-reservation.png" alt="予約"></a></li>
           <li><a href="access"><img src="<?php echo get_template_directory_uri(); ?>/images/txt-sidebar-access.png" alt="交通"></a></li>
-          <li><a href=""><img src="<?php echo get_template_directory_uri(); ?>/images/txt-sidebar-blog.png" alt="ブログ"></a></li>
+          <li><a href="blog"><img src="<?php echo get_template_directory_uri(); ?>/images/txt-sidebar-blog.png" alt="ブログ"></a></li>
         </ul>
       </div>
     </div>
