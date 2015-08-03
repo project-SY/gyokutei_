e134f861404adad8a2c84b9b2ef09053be5fa364
山﨑　旭 <b2w51134.asahi@gmail.com>
2015-08-03 14:21:49 +0900

    フォルダ追加

---
+++ coding_/access.php
@@ -0,0 +1,16 @@
+<?php include('head.php'); ?>
+	<?php include('nav.php'); ?>
+	<div id="contents" class="access">
+		<div class="kv-same kv-access"></div>
+		<img src="./images/icon-same.png" alt="アイコン" class="icon-same pc">
+		<img src="./images/txt-access-caption.png" alt="お風呂" class="caption pc">
+		<p class="caption-p">茶事は一年を通じ、自然の移り変わりや季節に応じて催されます。新しい茶を使い<br />始める初冬の時期の「口切」の茶から、「古茶」と呼ぶ</p>
+		<div class="access-route"></div>
+		<div class="access-subhead access-subhead-route"></div>
+		<div id="map"></div>
+		<div class="access-subhead access-subhead-info"></div>
+		<p class="access-about">
+			TEL:&nbsp;0560-85-8501<br />
+			250-0311&nbsp;&nbsp;神奈川県足柄下郡箱根町湯本501
+		</p>
+<?php include('footer.php'); ?>
---
+++ coding_/info.php
@@ -0,0 +1,37 @@
+<?php include('head.php'); ?>
+	<?php include('nav.php'); ?>
+	<div id="contents" class="info">
+		<div class="kv-same kv-info"></div>
+		<img src="./images/icon-same.png" alt="アイコン" class="icon-same pc">
+		<img src="./images/txt-info-caption.png" alt="お風呂" class="caption pc">
+		<p class="caption-p">茶事は一年を通じ、自然の移り変わりや季節に応じて催されます。新しい茶を使い<br />始める初冬の時期の「口切」の茶から、「古茶」と呼ぶ</p>
+		<div class="info-articles">
+			<!--ループ処理 start-->
+			<div class="info-articles-article article-1">
+				<h4>8月限定「伊勢海老プラン」</h4>
+				<time>2015/7/26</time>
+				<p>
+					お正月のおせち料理をブログで紹介してから、気がついたら、すでに一月も終わり。
+					なんと一月まだ一回しかブログ更新していないなんて。。。いや、ほんとは、書きたいことや、伝えたい事、たくさ〜んあるんだけど、なんせ、仕事が終わって寮に戻ってご飯食べたら、もう眠くって眠くって、ブログどころの騒ぎじゃないんだわ〜・・・・
+				</p>
+			</div>
+			<!--ループ処理 end-->
+			<!--ループ処理 start-->
+			<div class="info-articles-article">
+				<h4>8月限定「伊勢海老プラン」</h4>
+				<time>2015/7/26</time>
+				<p>
+					お正月のおせち料理をブログで紹介してから、気がついたら、すでに一月も終わり。
+					なんと一月まだ一回しかブログ更新していないなんて。。。いや、ほんとは、書きたいことや、伝えたい事、たくさ〜んあるんだけど、なんせ、仕事が終わって寮に戻ってご飯食べたら、もう眠くって眠くって、ブログどころの騒ぎじゃないんだわ〜・・・・
+				</p>
+			</div>
+			<!--ループ処理 end-->
+			<div class="info-articles-pager">
+				<ul class="clearfix">
+					<li><a href=""><img class="info-back-left" src="./images/icon-info-left-allow.png" alt="">過去の投稿</a></li>
+					<li><a href="">新しい投稿<img class="info-next-right" src="./images/icon-info-right-allow.png" alt=""></a></li>
+				</ul>
+			</div>
+		</div>
+
+<?php include('footer.php'); ?>
