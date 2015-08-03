c70ffd4f45b5af2cf94556f2d1c29304ef4577b1
Ken Sayama <sayamaken0402@gmail.com>
2015-08-03 14:30:03 +0900

    フォルダ追加

---
+++ coding_/js/common.js
@@ -0,0 +1,48 @@
+$(function(){
+	var window_w = $(window).width();
+	var window_h = $(window).height();
+
+	if (window_w > 569) {
+		//View on PC function
+		$(document).ready(function(){
+	 	 $('#bxslider').bxSlider();
+		});
+		//未完処理
+
+	} else if(window_w < 568) {
+		//View on SP function
+		//sidr-right
+		$(document).ready(function() {
+		    $('.btn').sidr({
+		      name: 'sidr-right',
+		      side: 'right',
+		      speed:500,
+		      displace: false
+		    });
+		  });
+		//SP menu height
+		var WrapperHeight = $('#wrapper').height();
+		var NavHeight = WrapperHeight - 50;
+		$(window).load(function () {
+	    $("#nav .inner").css("height", NavHeight+"px")
+		});
+
+		//SP menu btn
+		//$(window).load(function(){
+		//	$(".btn").click(function(){
+		//		$("#sidr-right").css("height",window_h+"px");
+		//		if ($("#wrapper").hasClass("no-active")) {
+		//			$("#wrapper").addClass("active").removeClass("no-active");
+		//			$("")
+		//		} else if ($("#wrapper").hasClass("active")) {
+		//			$("#wrapper").removeClass("active").addClass('no-active');
+		//		};;
+		//	});
+		//});
+		//pagetop
+		$(".pagetop").click(function () {
+			$("html,body").animate({ scrollTop: 0 }, "slow");
+			return false;
+		});
+	};
+});
---
+++ coding_/meta.php
@@ -0,0 +1,22 @@
+<head>
+<meta charset="UTF-8" />
+<title>玉庭</title>
+<meta name="keywords" content="###">
+<meta name="description" content="###" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0" />
+<meta property="og:type" content="###" />
+<meta property="og:url" content="###" />
+<meta property="og:title" content="###" />
+<meta property="og:description" content="###" />
+<meta property="og:image" content="###" />
+<link rel="shortcut icon" href="favicon.ico" />
+<link rel="stylesheet" href="./css/reset.css">
+<link rel="stylesheet" href="./css/style.css">
+<link rel="stylesheet" href="./css/jquery.bxslider.css">
+<script src="./js/jquery-1.11.3.min.js"></script>
+<script src="./js/jquery.sidr.min.js"></script>
+<script type="text/javascript" src="./js/jquery.bxslider.js"></script>
+<script src="http://www.google.com/jsapi/"></script>
+<script type="text/javascript" src="./js/map.js"></script>
+<script src="./js/common.js"></script>
+</head>
