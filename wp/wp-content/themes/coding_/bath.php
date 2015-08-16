a2100f70d07f360e889c34dfac9d224630ec4e62
山﨑　旭 <b2w51134.asahi@gmail.com>
2015-08-16 14:20:19 +0900

    modefied all texts

--- wp/wp-content/themes/coding_/bath.php
+++ wp/wp-content/themes/coding_/bath.php
@@ -12,7 +12,10 @@ Template Name: bath
   	</div>
 		<img src="<?php bloginfo('template_url'); ?>/images/icon-same.png" alt="アイコン" class="icon-same pc">
 		<img src="<?php bloginfo('template_url'); ?>/images/txt-bath-caption.png" alt="お風呂" class="caption pc">
-		<p class="caption-p">茶事は一年を通じ、自然の移り変わりや季節に応じて催されます。新しい茶を使い<br />始める初冬の時期の「口切」の茶から、「古茶」と呼ぶ</p>
+		<p class="caption-p">お部屋のお風呂は、源泉掛け流しで、常時５７度前後のお湯が出ます。<br />
+			泉質はナトリウム・塩化物・硫酸塩温泉でpH８，４の美肌の湯で、箱根の泉質はとても良いと言われ、<br />
+			温泉療法としても使われています。箱根湯本の泉質は、とても良いと言われていて、玉庭の自家源泉は温泉療法としても使われております。泉質はナトリウム・塩化物硫酸塩温泉でｐＨ８，４の美肌の湯です。
+		</p>
 		<div class="media clearfix">
 			<div class="media-img">
    			<img src="<?php bloginfo('template_url'); ?>/images/pic-bath-img-01.png" alt="">
@@ -21,7 +24,10 @@ Template Name: bath
 				<img class="pc" src="<?php bloginfo('template_url'); ?>/images/border-same.png" alt="">
 				<h5>展望大浴場金殿について</h5>
 				<img src="<?php bloginfo('template_url'); ?>/images/border-same.png" alt="">
-				<p>茶事は一年を通じ、自然の移り変わりや季節に応じて催されます。新しい茶を使い始める初冬の時期の「口切」の茶から、「古茶」と呼ぶ前年の茶の残りを心おしみつつ使う晩秋の(名残の)茶まで、季節に応ずる「茶事」として「茶事」は一年を通じ、自然の移り変わりや季節に応じて催されます。新しい茶を使い始める初冬の時期の「口切」の茶から、「古茶」と呼ぶ前年の茶の残りを心おしみつつ使う晩秋の(名残の)茶まで、季節に応ずる。</p>
+				<p>
+						夕暮れ時、早朝に窓の外の山並みをながめながら、ひのきのあたたかな感触を味わいながらつかる温泉は、体の疲れを芯からいやしてくれます。<br />
+						金殿は小屋組天井になっており、桜の季節には、散った桜の花びらが舞い込んでお湯の上に浮かぶという風情は桜の時期だけのお楽しみです。
+				</p>
 			</div>
 		</div>
 		<div class="media clearfix">
@@ -30,9 +36,12 @@ Template Name: bath
 			</div>
 			<div class="media-text">
 				<img class="pc" src="<?php bloginfo('template_url'); ?>/images/border-same.png" alt="">
-				<h5>展望大浴場金殿について</h5>
+				<h5>源泉掛け流しの客室内風呂について</h5>
 				<img src="<?php bloginfo('template_url'); ?>/images/border-same.png" alt="">
-				<p>茶事は一年を通じ、自然の移り変わりや季節に応じて催されます。新しい茶を使い始める初冬の時期の「口切」の茶から、「古茶」と呼ぶ前年の茶の残りを心おしみつつ使う晩秋の(名残の)茶まで、季節に応ずる「茶事」として「茶事」は一年を通じ、自然の移り変わりや季節に応じて催されます。新しい茶を使い始める初冬の時期の「口切」の茶から、「古茶」と呼ぶ前年の茶の残りを心おしみつつ使う晩秋の(名残の)茶まで、季節に応ずる。</p>
+				<p>
+					お部屋の風呂は、源泉掛け流しで、温泉スイッチを押すと常時５７℃前後のお湯がでます。<br />
+					お客様が適温に調節してお入りいただけます。プライベートで心ゆくまで温泉をご堪能いただけます。
+				</p>
 			</div>
 		</div>
 		<div class="media clearfix">
@@ -41,9 +50,12 @@ Template Name: bath
 			</div>
 			<div class="media-text">
 				<img class="pc" src="<?php bloginfo('template_url'); ?>/images/border-same.png" alt="">
-				<h5>展望大浴場金殿について</h5>
+				<h5>源泉掛け流しの客室内風呂について</h5>
 				<img src="<?php bloginfo('template_url'); ?>/images/border-same.png" alt="">
-				<p>茶事は一年を通じ、自然の移り変わりや季節に応じて催されます。新しい茶を使い始める初冬の時期の「口切」の茶から、「古茶」と呼ぶ前年の茶の残りを心おしみつつ使う晩秋の(名残の)茶まで、季節に応ずる「茶事」として「茶事」は一年を通じ、自然の移り変わりや季節に応じて催されます。新しい茶を使い始める初冬の時期の「口切」の茶から、「古茶」と呼ぶ前年の茶の残りを心おしみつつ使う晩秋の(名残の)茶まで、季節に応ずる。</p>
+				<p>
+					お部屋の風呂は、源泉掛け流しで、温泉スイッチを押すと常時５７℃前後のお湯がでます。<br />
+					お客様が適温に調節してお入りいただけます。プライベートで心ゆくまで温泉をご堪能いただけます。
+				</p>
 			</div>
 		</div>
  <?php include('footer.php'); ?>
