1e6d3c5a2002b7d8b2bd9b982f891915eaa4d790
山﨑　旭 <b2w51134.asahi@gmail.com>
2015-08-24 02:01:24 +0900

    modefied commentform

--- wp/wp-content/themes/theme-gyokutei/comments.php
+++ wp/wp-content/themes/theme-gyokutei/comments.php
@@ -1,13 +1,13 @@
-<div class="comment-headline">コメント(<?php get_comments_number(); ?>)</div>
-<?php wp_list_comments('callback=mytheme_comment'); ?>
+<div class="comment-headline">コメント(2)</div>
+	 <?php wp_list_comments('callback=mytheme_comment'); ?>
 <?php
-$args = array(
+$comments_args = array(
 	'fields' => array(
 		'email'  => '<p class="comment-form-email">' .
-								'<label>メールアドレス<span class="red">（必須）</span></label><input id="email" name="email"' . ( $html5 ? 'type="email"' : 'type="text"' ) . ' value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />
+	              	'<label>お名前<span class="red">（必須）</span></label><input id="email" name="email" placeholder="メールアドレス" ' . ( $html5 ? 'type="email"' : 'type="text"' ) . ' value="' . esc_attr(  $commenter['comment_author_email'] ) . '" size="30"' . $aria_req . ' />
 	               </p>',
 		'author' => '<p class="comment-form-author">'.
-								'<label>お名前<span class="red">（必須）</span></label><input id="author" name="author" type="text" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' />
+	              	'<label>メールアドレス<span class="red">（必須）</span></label><input id="author" name="author" type="text" placeholder="お名前" value="' . esc_attr( $commenter['comment_author'] ) . '" size="30"' . $aria_req . ' />
 	               </p>',
 		'checkbox'    => '<div>
 												<input type="checkbox">
