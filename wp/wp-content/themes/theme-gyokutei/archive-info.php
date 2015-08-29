84ae69d49d4cd4aa981aab565e47b076bec29ced
山﨑　旭 <b2w51134.asahi@gmail.com>
2015-08-30 03:23:22 +0900

    modefied_custom post type

--- wp/wp-content/themes/theme-gyokutei/archive-info.php
+++ wp/wp-content/themes/theme-gyokutei/archive-info.php
@@ -16,9 +16,14 @@ Template Name: info
 いろいろな機会にご利用いただきたいと思います。</p>
 		<div class="info-articles">
 			<!--ループ処理 start-->
-				<?php $loop = new WP_Query(array("post_type" => "info","posts_per_page" => "10")); ?>
-         <?php if($loop->have_posts()): ?>
-         <?php while($loop->have_posts()): $loop->the_post(); ?>
+				<?php $paged = get_query_var('paged'); ?>
+				<?php query_posts( array(
+				'post_type' => 'info', //ここに対象のカスタム投稿名を入れます
+				'posts_per_page' => 4,
+				'paged' => $paged
+				)); ?>
+				<?php if(have_posts()): ?>
+         <?php while (have_posts()) : the_post(); ?>
          <?php if(is_odd_post()): ?>
 					<div class="info-articles-article bg-odd">
 						<h4><?php echo the_title(); ?></h4>
@@ -27,7 +32,7 @@ Template Name: info
 							<?php the_content(); ?>
 						</p>
 					</div>
-					<?php else: ?>
+				<?php else: ?>
 					<div class="info-articles-article">
 						<h4><?php echo the_title(); ?></h4>
 						<time><?php echo get_the_date(); ?></time>
